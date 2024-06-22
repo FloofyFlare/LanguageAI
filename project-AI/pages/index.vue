9aa5cb1749aed245275dd3a96a7eae166d582d12
Suzaku17 <c.hunt7264@gmail.com>
2024-06-22 19:35:54 -0400

    nav

--- project-AI/pages/index.vue
+++ project-AI/pages/index.vue
@@ -1,4 +1,34 @@
 <template>
+   <header>
+    <div class="">
+      <div class="navbar bg-primary">
+        <div class="avatar btn btn-ghost bg-info ">
+          <div class="w-40 pb-28">
+          <img class="" src="/images/logo.png" />
+          </div>
+        </div>
+        <div class="flex-none">
+          <ul class="menu menu-horizontal px-1 ml-48">
+            <li class>
+              <details>
+                <summary class="text-info">
+                  Other links
+                </summary>
+                <ul class="p-2 text-neutral bg-info rounded-t-none">
+                  <li><a>Link 1</a></li>
+                  <li><a>Link 2</a></li>
+                </ul>
+              </details>
+            </li>
+           
+          </ul>
+          <ul class="px-1 ml-48"></ul>
+          <li class="btn btn-ghost bg-primary text-info"><a>Sign up</a></li>
+        </div>
+        
+      </div>
+    </div>
+  </header>
   <div>
     <h1>Index</h1>
     <p>This is a basic index page.</p>

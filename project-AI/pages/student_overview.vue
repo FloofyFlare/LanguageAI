ca5582fddd917e1a218ca61a11ebb31203133db0
FloofyFlare <darkninja4347@yahoo.com>
2024-06-22 20:23:04 -0400

    nav bar

--- project-AI/pages/student_overview.vue
+++ project-AI/pages/student_overview.vue
@@ -1,25 +1,4 @@
 <template>
-  <header>
-    <div class="">
-      <div class="navbar bg-primary">
-        <div class="avatar btn btn-ghost bg-info ">
-          <div class="w-40 pb-28">
-          <img class="" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-          </div>
-        </div>
-        <div class="flex-none">
-         
-          
-          <li class="btn btn-ghost bg-primary text-info"><a>Home</a></li>
-        </div>
-        <div class="avatar">
-          <div class="w-24 rounded-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-          </div>
-        </div>
-      </div>
-    </div>
-  </header>
   <div>
     <h1>student</h1>
     <p>This is a basic index page.</p>

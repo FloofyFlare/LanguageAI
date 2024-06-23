0ba1c75b61fbb3cbc8a50c7297b4de75a4c868c1
FloofyFlare <darkninja4347@yahoo.com>
2024-06-22 20:44:27 -0400

    color

--- project-AI/tailwind.config.js
+++ project-AI/tailwind.config.js
@@ -4,4 +4,33 @@ module.exports = {
   plugins: [
     daisyui,
   ],
+  daisyui: {
+    themes: [
+        {
+          mytheme: {
+            primary: '#ED7624',
+  
+            secondary: '#0084DE',
+  
+            accent: '#FFE6E6',
+
+            'accent-contrast': '#E8E8FF',
+
+            'accent-2': '#FFFACE',
+
+            neutral: '#DEDEDE',
+  
+            'base-100': '#FFFFFF',
+  
+            info: '#000000',
+  
+            success: '#44E937',
+  
+            warning: '#ffe184',
+  
+            error: '#e44a4a',
+          },
+        },
+    ]
+  }, 
 }

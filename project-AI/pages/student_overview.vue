b7e8b131c2800a0d234ef934934279377601062e
FloofyFlare <darkninja4347@yahoo.com>
2024-07-09 22:26:20 -0400

    Animation of AI

--- project-AI/pages/student_overview.vue
+++ project-AI/pages/student_overview.vue
@@ -3,32 +3,32 @@
     <main>
       <div class="flex justify-center items-center">
         <div class="w-1/2 overflow-y-scroll border-l-2 h-full bg-base-100 pt-0 p-10 border-gray-300">
-              <div class="text-center justify-right 0">
-                <div class="flex gap-5 items-end justify-end flex-end">
-                  <p class="text-3xl font-bold">Timer:</p>
-                  <div>
-                    <span class="countdown font-mono text-4xl">
-                      <span style="--value:15;"></span>
-                    </span>
-                    minutes
-                  </div>
-                  <div>
-                    <span class="countdown font-mono text-4xl">
-                      <span style="--value:${counter};"></span>
-                    </span>
-                    seconds
-                  </div>
-                </div>
-              </div>
           <div class="text-center justify-right">
-            <div class="avatar">
-              <div class="w-24 rounded-full">
-                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            <div class="flex gap-5 items-end justify-center flex-end">
+              <p class="text-3xl font-bold">Timer:</p>
+              <div>
+                <span class="countdown font-mono text-4xl">
+                  <span style="--value:15;"></span>
+                </span>
+                minutes
+              </div>
+              <div>
+                <span class="countdown font-mono text-4xl">
+                  <span style="--value:${counter};"></span>
+                </span>
+                seconds
               </div>
             </div>
+          </div>
+          <div class="text-center justify-right">
+            <div class="avatar h-full w-full flex justify-center">
+              <div class="w-2/4 h-2/4 ">
+                <div class="h-full w-full rounded-full bg-orange-500 p-2 shadow-2xl rounded-full border-8 border-gray-300 p-2" id="ai_animation"></div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-      <div class="w-1/2 overflow-y-scroll border-l-2 h-full bg-base-100 pt-0 p-10 border-gray-300 bg-warning">
+        <div class="w-1/2 overflow-y-scroll border-l-2 h-full bg-base-100 pt-0 p-10 border-gray-300 bg-warning">
           <div class="chat chat-start">
             <div class="chat-image avatar">
               <div class="w-10 rounded-full">
@@ -45,9 +45,7 @@
           <div class="chat chat-end">
             <div class="chat-image avatar">
               <div class="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS chat bubble component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                <div class="h-full w-full rounded-full bg-orange-500 p-2"></div>
               </div>
             </div>
             <div class="chat-header">
@@ -71,9 +69,7 @@
           <div class="chat chat-end">
             <div class="chat-image avatar">
               <div class="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS chat bubble component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                <div class="h-full w-full rounded-full bg-orange-500 p-2"></div>
               </div>
             </div>
             <div class="chat-header">
@@ -97,9 +93,7 @@
           <div class="chat chat-end">
             <div class="chat-image avatar">
               <div class="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS chat bubble component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                <div class="h-full w-full rounded-full bg-orange-500 p-2"></div>
               </div>
             </div>
             <div class="chat-header">
@@ -123,9 +117,7 @@
           <div class="chat chat-end">
             <div class="chat-image avatar">
               <div class="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS chat bubble component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                <div class="h-full w-full rounded-full bg-orange-500 p-2"></div>
               </div>
             </div>
             <div class="chat-header">
@@ -149,9 +141,7 @@
           <div class="chat chat-end">
             <div class="chat-image avatar">
               <div class="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS chat bubble component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                <div class="h-full w-full rounded-full bg-orange-500 p-2"></div>
               </div>
             </div>
             <div class="chat-header">
@@ -175,9 +165,7 @@
           <div class="chat chat-end">
             <div class="chat-image avatar">
               <div class="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS chat bubble component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                <div class="h-full w-full rounded-full bg-orange-500 p-2"></div>
               </div>
             </div>
             <div class="chat-header">

07fa88de7682455cdc3a21c03725984ae0b40d29
FloofyFlare <darkninja4347@yahoo.com>
2024-07-14 23:01:58 -0400

    Made it responsive to simple input

--- project-AI/pages/student_overview.vue
+++ project-AI/pages/student_overview.vue
@@ -28,156 +28,26 @@
             </div>
           </div>
           <div class="text-center justify-right">
-            <button class="btn bg-info text-neutral">
-              <NuxtImg :src="'/images/mic.png'" class="w-10 h-10"></NuxtImg>
-              Speak
+            <button class="btn btn-circle h-full p-4 w-1/4 bg-info text-neutral shadow-2xl">
+              <NuxtImg :src="'/images/mic.png'" class="w-20 h-20"></NuxtImg>
             </button>
           </div>
         </div>
-        <div class="w-1/2 overflow-y-scroll border-l-2 h-full bg-base-100 pt-0 p-10 border-gray-300 bg-warning">
-          <div class="chat chat-start">
+        <div class="w-1/2 overflow-y-scroll border-l-2 h-full min-h-screen bg-base-100 pt-0 p-10 border-gray-300 bg-warning">
+          <div v-for="{ id, sender, message } in chatHistory" :key="id" :class="`chat ${chatSide(sender)}`">
             <div class="chat-image avatar">
               <div class="w-10 rounded-full">
+                <div v-if="sender === 'ai'" class="h-full w-full rounded-full bg-orange-500 p-2"></div>
                 <img
+                  v-if="sender !== 'ai'"
                   alt="Tailwind CSS chat bubble component"
                   src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
               </div>
             </div>
             <div class="chat-header">
-              Tony
+              {{sender}}
             </div>
-            <div class="chat-bubble chat_enter">abdfglsaf</div>
-          </div>
-          <div class="chat chat-end">
-            <div class="chat-image avatar">
-              <div class="w-10 rounded-full">
-                <div class="h-full w-full rounded-full bg-orange-500 p-2"></div>
-              </div>
-            </div>
-            <div class="chat-header">
-              AI
-            </div>
-            <div class="chat-bubble chat_enter">fdsfahasdfldw</div>
-          </div>
-          <div class="chat chat-start">
-            <div class="chat-image avatar">
-              <div class="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS chat bubble component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-              </div>
-            </div>
-            <div class="chat-header">
-              Tony
-            </div>
-            <div class="chat-bubble chat_enter chat_enter">abdfglsaf</div>
-          </div>
-          <div class="chat chat-end">
-            <div class="chat-image avatar">
-              <div class="w-10 rounded-full">
-                <div class="h-full w-full rounded-full bg-orange-500 p-2"></div>
-              </div>
-            </div>
-            <div class="chat-header">
-              AI
-            </div>
-            <div class="chat-bubble chat_enter">fdsfahasdfldw</div>
-          </div>
-          <div class="chat chat-start">
-            <div class="chat-image avatar">
-              <div class="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS chat bubble component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-              </div>
-            </div>
-            <div class="chat-header">
-              Tony
-            </div>
-            <div class="chat-bubble chat_enter">abdfglsaf</div>
-          </div>
-          <div class="chat chat-end">
-            <div class="chat-image avatar">
-              <div class="w-10 rounded-full">
-                <div class="h-full w-full rounded-full bg-orange-500 p-2"></div>
-              </div>
-            </div>
-            <div class="chat-header">
-              AI
-            </div>
-            <div class="chat-bubble chat_enter">fdsfahasdfldw</div>
-          </div>
-          <div class="chat chat-start">
-            <div class="chat-image avatar">
-              <div class="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS chat bubble component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-              </div>
-            </div>
-            <div class="chat-header">
-              Tony
-            </div>
-            <div class="chat-bubble chat_enter">abdfglsaf</div>
-          </div>
-          <div class="chat chat-end">
-            <div class="chat-image avatar">
-              <div class="w-10 rounded-full">
-                <div class="h-full w-full rounded-full bg-orange-500 p-2"></div>
-              </div>
-            </div>
-            <div class="chat-header">
-              AI
-            </div>
-            <div class="chat-bubble chat_enter">fdsfahasdfldw</div>
-          </div>
-          <div class="chat chat-start">
-            <div class="chat-image avatar">
-              <div class="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS chat bubble component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-              </div>
-            </div>
-            <div class="chat-header">
-              Tony
-            </div>
-            <div class="chat-bubble chat_enter">abdfglsaf</div>
-          </div>
-          <div class="chat chat-end">
-            <div class="chat-image avatar">
-              <div class="w-10 rounded-full">
-                <div class="h-full w-full rounded-full bg-orange-500 p-2"></div>
-              </div>
-            </div>
-            <div class="chat-header">
-              AI
-            </div>
-            <div class="chat-bubble chat_enter">fdsfahasdfldw</div>
-          </div>
-          <div class="chat chat-start">
-            <div class="chat-image avatar">
-              <div class="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS chat bubble component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-              </div>
-            </div>
-            <div class="chat-header">
-              Tony
-            </div>
-            <div class="chat-bubble chat_enter">abdfglsaf</div>
-          </div>
-          <div class="chat chat-end">
-            <div class="chat-image avatar">
-              <div class="w-10 rounded-full">
-                <div class="h-full w-full rounded-full bg-orange-500 p-2"></div>
-              </div>
-            </div>
-            <div class="chat-header">
-              AI
-            </div>
-            <div class="chat-bubble chat_enter">fdsfahasdfldw</div>
+            <div class="chat-bubble chat_enter">{{message}}</div>
           </div>
         </div>
       </div>
@@ -187,5 +57,41 @@
   </body>
 </template>
 <script setup lang="ts">
+  import { ref } from 'vue';
 
+  const order = 0;
+
+  interface ChatMessage {
+    id: number;
+    sender: string;
+    message: string;
+  }
+
+  function chatSide(params:string) {
+      if (params == 'ai') {
+        return 'chat-end';
+      } else {
+        return 'chat-start';
+      }
+  }
+
+  const chatHistory = ref<ChatMessage[]>([]);
+
+  testInput();
+  function testInput() {
+    chatHistory.value = [
+      { id: order, sender: 'user', message: 'hello' },
+      { id: order, sender: 'ai', message: 'how are you?' },
+    ];
+  }
+
+  const sendMessage = () => {
+    const userMessage = userMessageInput.value;
+    if (userMessage) {
+      chatHistory.value.push({ id: order, sender: 'user', message: userMessage });
+      userMessageInput.value = '';
+    }
+  };
+
+  const userMessageInput = ref('');
 </script>
